3cecb25cc2d475c34ae36cb5a17725cc33c4ab7d
YoBii <YoBii@users.noreply.github.com>
2025-01-07 21:00:40 +0100

    Alternative box highlighting and improved border highlighting (#2)
    * draw border inwards by scaling rect
    
    * add alternative box highlighting; add box settings; reorganize settings to submenus

--- WaystoneHighlightSettings.cs
+++ WaystoneHighlightSettings.cs
@@ -71,36 +71,68 @@ public class ScoreSettings
 }
 
 [Submenu(CollapsedByDefault = false)]
-public class GraphicSettings
-{
-//BORDER COLOR
-   [Menu("Runnable Waystone Border Color", "Color of the Border of Runnable Waystones")]
-    public ColorNode RunBorderColor { get; set; } = new ColorNode(Color.Green);
+public class GraphicSettings {
+    //HIGHLIGHT COLOR
+    [Menu("Runnable Waystone Highlight Color", "Highlight color for Runnable Waystones")]
+    public ColorNode RunHighlightColor { get; set; } = new ColorNode(Color.Green);
 
-   [Menu("Craftable Waystone Border Color", "Color of the Border of Craftable Waystones")]
-    public ColorNode CraftBorderColor { get; set; } = new ColorNode(Color.Yellow);
+    [Menu("Craftable Waystone Highlight Color", "Highlight color for Craftable Waystones")]
+    public ColorNode CraftHighlightColor { get; set; } = new ColorNode(Color.Yellow);
 
-   [Menu("Banned Modifiers Waystone Border Color", "Color of the Border of Waystones with Banned Modifiers")]
-    public ColorNode BannedBorderColor { get; set; } = new ColorNode(Color.Red);
+    [Menu("Banned Modifiers Waystone Highlight Color", "Highlight color for Waystones with Banned Modifiers")]
+    public ColorNode BannedHighlightColor { get; set; } = new ColorNode(Color.Red);
 
-//BORDER THICKNESS
-   [Menu("Runnable Waystone Border Thickness", "Thickness of the Border of Runnable Waystones")]
+    //HIGHLIGHT STYLE
+    [Menu("Runnable Waystone Highlight Style", "1 = Border; 2 = Filled box/dot")]
+    public RangeNode<int> RunHightlightStyle { get; set; } = new RangeNode<int>(1, 1, 2);
+    [Menu("Craftable Waystone Highlight Style", "1 = Broder; 2 = Filled box/dot")]
+    public RangeNode<int> CraftHightlightStyle { get; set; } = new RangeNode<int>(1, 1, 2);
+    [Menu("Banned Waystone Highlight Style", "1 = Border; 2 = Filled box/dot")]
+    public RangeNode<int> BannedHightlightStyle { get; set; } = new RangeNode<int>(1, 1, 2);
+
+    [Menu("Border Highlight Thickness Settings")]
+    public BorderHighlightSettings BorderHighlight { get; set; } = new BorderHighlightSettings();
+    [Menu("Box Highlight Rounding Settings")]
+    public BoxHighlightSettings BoxHighlight { get; set; } = new BoxHighlightSettings();
+    [Menu("Font Size Settings")]
+    public FontSizeSettings FontSize { get; set; } = new FontSizeSettings();
+}
+
+[Submenu(CollapsedByDefault = true)]
+public class BorderHighlightSettings {
+    //BORDER THICKNESS
+    [Menu("Runnable Waystone Border Thickness", "Thickness of the Border of Runnable Waystones")]
     public RangeNode<int> RunBorderThickness { get; set; } = new RangeNode<int>(1, 1, 5);
-  
-   [Menu("Craftable Waystone Border Thickness", "Thickness of the Border of Craftable Waystones")]
+
+    [Menu("Craftable Waystone Border Thickness", "Thickness of the Border of Craftable Waystones")]
     public RangeNode<int> CraftBorderThickness { get; set; } = new RangeNode<int>(1, 1, 5);
-  
-   [Menu("Banned Modifiers Waystone Border Thickness", "Thickness of the Border of Waystones with Banned Modifiers")]
+
+    [Menu("Banned Modifiers Waystone Border Thickness", "Thickness of the Border of Waystones with Banned Modifiers")]
     public RangeNode<int> BannedBorderThickness { get; set; } = new RangeNode<int>(1, 1, 5);
+}
 
+[Submenu(CollapsedByDefault = true)]
+public class BoxHighlightSettings {
+    //BOX ROUNDING
+    [Menu("Runnable Waystone Box Rounding", "Rounding of the Box for Runnable Waystones")]
+    public RangeNode<int> RunBoxRounding { get; set; } = new RangeNode<int>(1, 1, 60);
 
-//FONT SIZE (Needs modifications as text scales from origin point and doesn't change position accordingly)
-   [Menu("Waystone Quantity+Rarity Font Size", "Size of the global font for Waystone Scores")]
+    [Menu("Craftable Waystone Box Rounding", "Rounding of the Box for Craftable Waystones")]
+    public RangeNode<int> CraftBoxRounding { get; set; } = new RangeNode<int>(1, 1, 60);
+
+    [Menu("Banned Modifiers Waystone Box Rounding", "Rounding of the Box for Waystones with Banned Modifiers")]
+    public RangeNode<int> BannedBoxRounding { get; set; } = new RangeNode<int>(1, 1, 60);
+}
+
+[Submenu(CollapsedByDefault = true)]
+public class FontSizeSettings {
+    //FONT SIZE (Needs modifications as text scales from origin point and doesn't change position accordingly)
+    [Menu("Waystone Quantity+Rarity Font Size", "Size of the global font for Waystone Scores")]
     public RangeNode<float> QRFontSizeMultiplier { get; set; } = new RangeNode<float>(1.0f, 0.5f, 2f);
 
-  [Menu("Waystone Score Font Size", "Size of the global font for Waystone Scores")]
+    [Menu("Waystone Score Font Size", "Size of the global font for Waystone Scores")]
     public RangeNode<float> ScoreFontSizeMultiplier { get; set; } = new RangeNode<float>(1.0f, 0.5f, 2f);
 
-  [Menu("Waystone Prefix+Suffix Font Size", "Size of the global font for Affix Count")]
+    [Menu("Waystone Prefix+Suffix Font Size", "Size of the global font for Affix Count")]
     public RangeNode<float> PrefSuffFontSizeMultiplier { get; set; } = new RangeNode<float>(1.0f, 0.5f, 2f);
 }
